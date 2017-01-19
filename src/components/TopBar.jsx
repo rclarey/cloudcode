d1a45e42e6f6e9b46488a3c4f691195e01d58925
rclarey <r.e.clarey@gmail.com>
2017-01-19 15:01:49 -0500

    created needed components

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,15 @@
+// src/components/TopBar.jsx
+
+import React from 'react';
+
+export default TopBar = React.createClass({
+
+  render() {
+    <div>
+      TopBar
+    </div>
+  }
+
+});
+
+TopBar.propTypes = {};
---
+++ src/components/workspace/BottomBar.jsx
@@ -0,0 +1,15 @@
+// src/components/workspace/BottomBar.jsx
+
+import React from 'react';
+
+export default BottomBar = React.createClass({
+
+  render() {
+    <div>
+      BottomBar
+    </div>
+  }
+
+});
+
+BottomBar.propTypes = {};
