a77e9fcba9562e59e30b4b62a1a5d203320e526d
rclarey <r.e.clarey@gmail.com>
2017-01-20 22:08:24 -0500

    add codemirror + styles for it

--- src/components/workspace/Editor.jsx
+++ src/components/workspace/Editor.jsx
@@ -3,10 +3,13 @@
 import React from 'react';
 
 const Editor = React.createClass({
+  componentDidMount() {
+    CodeMirror(document.querySelector('#workspace-editor'), { lineNumbers: true, scrollbarStyle: 'null', autofocus: true });
+  },
 
   render() {
-    return (<div id="workspace-editor">Editor</div>);
-  }
+    return (<div id="workspace-editor" />);
+  },
 
 });
 
